7dbdb06824fc20b61448544643bae3d0f1c4b43d
toshiemon18 <st12318@gmail.com>
2014-01-05 03:08:02 +0900

    いろいろ変えた

--- main.rb
+++ main.rb
@@ -1,48 +1,46 @@
 #coding : utf-8
-require 'tweetstream'
-require 'open-uri'
-require 'gtk2'
-require 'json'
 
-<<<<<<< HEAD
+require "rubygems"
+require "tweetstream"
+require "open-uri"
+require "gtk2"
+require "json"
+
+keys = JSON.parse(File.read("./keys/keys.json"))["twitter"]
+
 TweetStream.configure do |cnf| #OAuthに必要な情報類
-	cnf.consumer_key = "" #コンシューマキー
-	cnf.consumer_secret = "" #コンシューマシークレット
-	cnf.oauth_token = "" #OAuthトークン
-	cnf.oauth_token_secret = "" #OAuthトークンシークレット
-=======
-TweetStream.configure do |cnf|
-	cnf.consumer_key = ""
-	cnf.consumer_secret = ""
-	cnf.oauth_token = ""
-	cnf.oauth_token_secret = ""
->>>>>>> 679f98b1450a4dce2dbc0ee74428de574548e091
+	cnf.consumer_key = keys["consumer_key"] #コンシューマキー
+	cnf.consumer_secret = keys["consumer_secret"] #コンシューマシークレット
+	cnf.oauth_token = keys["token"] #OAuthトークン
+	cnf.oauth_token_secret = keys["token_secret"] #OAuthトークンシークレット
 	cnf.auth_method = :oauth
 end
 
-Thread.fork do
-	window = window = Gtk::Window.new
-	window.title = 'JoshoLT'
+TweetStream::Client.new.track("#JoshoLT") do |status|
+	puts "#{status.user.name} : #{status.text}"
 
-	#windowをdelete_eventに接続
-	window.signal_connect("delete_event") do
-		Gtk.main_quit#delete_eventが発行されたらメインループを終了
+	time_line = Thread.new do
+		window = window = Gtk::Window.new
+		window.title = "JoshoLT #" + keys["times"]
+
+		#windowをdelete_eventに接続
+		window.signal_connect("delete_event") do
+			Gtk.main_quit#delete_eventが発行されたらメインループを終了
+		end
+
+		window_size_request(500, 715)#ウィンドウサイズを500*715に設定
+
+		#スクロールできるようなウィジェットを生成
+		scroller = Gtk::ScrolledWindow.new(hadjusment = nil, vadjusment = nil)#上下スクロールできるコンテナウィジェット
+		scroller.set_size_request(500, 715)
+		scroller.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS)
+
+		#windowにスクローラウィンドを格納する
+		window.add(scroller)
+		window.show_all
+		Gtk.main
 	end
 
-	main_quit.set_size_request(500, 715)#ウィンドウサイズを500*715に設定
-
-	#スクロールできるようなウィジェットを生成
-	scroller = Gtk::ScrolledWindow.new(hadjusment = nil, vadjusment = nil)#上下スクロールできるコンテナウィジェット
-	scroller.set_size_request(500, 715)
-	scroller.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS)
-
-	#windowにスクローラウィンドを格納する
-	window.add(scroller)
-	window.show_all
-end
-
-TweetStream::Client.new.track('#JoshoLT') do |status|
-	puts "#{status.user.name} : #{status.text}"
 	#tweetを格納した垂直ボックスを作成する
 	name = Gtk::Label.new(status.user.name) #ユーザーネーム用ラベル
 	screen_name = Gtk::Label.new(status.user.screen_name)#スクリーンネーム用ラベル
@@ -63,17 +61,7 @@ TweetStream::Client.new.track('#JoshoLT') do |status|
 	twicell.pack_end(body, false, false, 5)
 	twicell.pack_end(name, false, false, 5)
 
-<<<<<<< HEAD
 	#スクローラウィンドにtwicellを格納
 	scroller.add_with_viewport(twicell)
 	window.show_all
-	Gtk.main
 end
-=======
-		window.show_all
-	end
-end
-
-time_line.run
-streamer.run
->>>>>>> 679f98b1450a4dce2dbc0ee74428de574548e091
